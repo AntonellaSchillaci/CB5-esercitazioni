a998e78c0c9586f2845a2e4b970e4e6ee0e43df9
Antonella Schillaci <antonella.schillaci145@gmail.com>
2022-11-13 17:38:35 +0100

    esercitazione 11-11-22

--- 11-11-22/script.js
+++ 11-11-22/script.js
@@ -1,4 +1,4 @@
-const cardEl = document.getElementById("card");
+const cardEl = document.querySelector(".card");
 
 const imgEl = document.createElement("img");
 imgEl.className = "pokemon_img";
@@ -12,53 +12,52 @@ idEl.className = "pokemon_id";
 const typeEl = document.createElement("p");
 typeEl.className = "pokemon_type";
 
-const btnPrev = document.querySelector(".prev");
-const btnNext = document.querySelector(".next");
+const buttonPrev = document.querySelector(".prev");
+const buttonNext = document.querySelector(".next");
 
 let index = 1;
 
-btnNext.addEventListener("click", () => {
+buttonNext.addEventListener("click", () => {
     if (index >= 1) {
-        btnPrev.classList.remove("inactive");
+        buttonPrev.classList.remove();
     }
     index ++;
     pokemon(index);
 });
 
-btnPrev.addEventListener("click", () => {
-    if(index === 1) {
-    return;
+buttonPrev.addEventListener("click", () => {
+    if (index === 1) {
+        return;
     }
     index --;
     pokemon(index);
 })
 
 function createId (id) {
-    if ( !id ) return null;
-    if ( id < 10 ) {return `00${id}`}
-    else if ( id < 100 ) {return `0${id}`}
+    if (!id) return null;
+    if (id < 10) {return `00${id}`}
+    else if (id < 100) {return `0${id}`}
     return id;
 }
 
-const createCard = (el) => {
+const createCard = (data) => {
 
-    cardEl.className = (el.types[0].type.name);
-    
-    imgEl.setAttribute("src" , el.sprites.other.dream_world.front_default);
-    nameEl.textContent = el.name;
-    idEl.textContent = `#${createId(el.id)}`;
-    typeEl.textContent = `Type: ${el.types[0].type.name}`;
+cardEl.className = (data.types[0].type.name);
 
-    cardEl.append(imgEl, nameEl, idEl, typeEl);
+imgEl.setAttribute("src", data.sprites.other.dream_world.front_default);
+nameEl.textContent = data.name;
+idEl.textContent = `#${createId(data.id)}`;
+typeEl.textContent = `Type: ${data.types[0].type.name}`;
+
+cardEl.append(imgEl, nameEl, idEl, typeEl);
 }
-
+const caric = document.querySelector (".loading")
 
 const pokemon = async (i) => {
-    cardEl.classList.toggle("active");
+    //caric.classList.toggle("active"); ---?
     let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
-    let data = await response.json();
-    cardEl.classList.toggle("active");
-    createCard(data);
+    let x = await response.json();
+    createCard(x);
 }
 
 pokemon(index);
