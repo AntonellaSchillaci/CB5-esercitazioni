cb77296fee8ed43066f0edd619771fa123516b04
Antonella Schillaci <antonella.schillaci145@gmail.com>
2023-01-31 16:47:27 +0100

    esercitazione 31-01-23

--- router-react/src/App.jsx
+++ router-react/src/App.jsx
@@ -1,16 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { FaUserAlt } from 'react-icons/fa';
+import { BsSignpost2Fill } from 'react-icons/bs'
 import styles from './App.module.scss';
 
 
 function App() {
   return (
-    <div className={styles.App}>
-      <h1>Hello, view the users</h1>
-      <div className={styles.link}>
-        <Link to="/users"><FaUserAlt /></Link>
+    <>
+      <div className={styles.App}>
+        <h1>Hi, what do you want to see today?</h1>
+        <div className={styles.link}>
+          <Link to="/users"><FaUserAlt /> Users</Link>
+          <Link to="/posts"><BsSignpost2Fill /> Posts</Link>
+        </div>
       </div>
-    </div>
+      <Outlet />
+    </>
+    
   );
 }
 
--- router-react/src/routes/user.jsx
+++ router-react/src/routes/user.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, Outlet} from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { ImHome } from 'react-icons/im';
@@ -18,16 +18,19 @@ export default function User() {
   }, [user]);
 
   return (
-    <div className={styles.User} id={userData.id}>
-      <div className={styles.content}>
-        <img src={userData.image} alt={userData.username}></img>
-        <h3>{userData.firstName} <br></br> {userData.lastName}</h3>
-        <h5> Age: {userData.age}</h5>
-        <div className={styles.btnContainer}>
-        <button className={styles.buttonHome} onClick={() => { navigate("/") }}><ImHome /></button>
-        <button className={styles.buttonHome} onClick={() => { navigate("../users") }}><FaUserAlt /></button>
+    <>
+      <div className={styles.User} id={userData.id}>
+        <div className={styles.content}>
+          <img src={userData.image} alt={userData.username}></img>
+          <h3>{userData.firstName} <br></br> {userData.lastName}</h3>
+          <h5> Age: {userData.age}</h5>
+          <div className={styles.btnContainer}>
+            <button className={styles.buttonHome} onClick={() => { navigate("/"); } }><ImHome /></button>
+            <button className={styles.buttonHome} onClick={() => { navigate("../users"); } }><FaUserAlt /></button>
+          </div>
+        </div>
       </div>
-      </div>
-    </div>
+      <Outlet />
+    </>
   );
 }
