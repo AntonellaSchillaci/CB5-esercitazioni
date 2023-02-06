a00f0d8478994b5b70bfd7072a6a70fef554e703
Antonella Schillaci <antonella.schillaci145@gmail.com>
2023-02-06 16:03:27 +0100

    esercitazione 06-02-23

---
+++ react-global-state/src/App.jsx
@@ -0,0 +1,16 @@
+import Counter from './components/counter'
+
+import styles from './App.module.scss'
+
+
+function App() {
+
+  return (
+    <div className={styles.App}>
+      <h1>Counter</h1>
+      <Counter />
+    </div>
+  )
+}
+
+export default App
